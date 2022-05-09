3b580b7fa53c2e1edcf5202d9ca8b2a4fb53f375
Alexandre Maeda <ale.b.maeda@gmail.com>
2022-05-09 17:48:11 -0300

    nodemon

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("online"));
+app.get("/", (req, res) => res.json({ message: "Online!" }));
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log("Backend Started!👌");
+});
