541e65ce2edadb3bff15b53ef9f343f9f48d1eb5
Alexandre Maeda <ale.b.maeda@gmail.com>
2022-05-17 14:22:47 -0300

    comment fix

--- backend-ts/project-2/src/shared/infra/typeorm/data-source.ts
+++ backend-ts/project-2/src/shared/infra/typeorm/data-source.ts
@@ -1,6 +1,6 @@
 import { DataSource } from 'typeorm';
 //docker-compose exec app node --require ts-node/register ./node_modules/typeorm/cli.js migration:run -d src/database
-//yarn typeorm migration:create src/database/migrations/CreateCategories
+// yarn typeorm migration:create src/shared/infra/typeorm/migrations/CreateCars
 // docker run --name rentalx_database -e POSTGRES_USER=docker -e POSTGRES_PASSWORD=ignite -e POSTGRES_DB=rentalx -p 5432:5432 -d postgres
 
 export const AppDataSource = new DataSource({
