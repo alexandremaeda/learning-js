a75cbd58771766a4ab7a71c5dccb3ed2dbf66889
Alexandre Maeda <ale.b.maeda@gmail.com>
2022-05-18 14:18:21 -0300

    config auth

---
+++ backend-ts/project-2/src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: '43eef7ba0de84d4bc341421663484b8a',
+  expires_in_token: '15m',
+  secret_refresh_token: '65f9c2d174ff38ead38339d7dec2389c',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+};
